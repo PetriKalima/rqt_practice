22052196f24feb0b0c70d4f813336a089db9d84d
Petri Kalima <petri.kalima@aalto.fi>
2020-01-14 16:37:20 +0200

    add rest of primitives

--- src/rqt_mypkg/testWidget.py
+++ src/rqt_mypkg/testWidget.py
@@ -6,7 +6,8 @@ from PyQt5.QtGui import QColor, QPalette, QPixmap, QCursor, QFont
 from time import sleep
 from functools import partial
 
-from panda_pbd.msg import MoveToEEGoal, MoveToContactActionGoal
+from panda_pbd.msg import MoveToEEGoal, MoveToContactActionGoal, UserSyncGoal
+from panda_pbd.srv import MoveFingersRequest, ApplyForceFingersRequest
 
 from panda_eup.pbd_interface import PandaPBDInterface
 import panda_eup.panda_primitive as pp
@@ -21,9 +22,15 @@ class TestWidget(QWidget):
         self.programThreadPool = QThreadPool()
         self.VBox = QVBoxLayout(self)
         self.VBox.setAlignment(Qt.AlignTop)
-        #self.VBox.addWidget(self.buttonWidget)
         self.tabs = MyTabWidget(self)
         self.VBox.addWidget(self.tabs)
+        self.primitiveList = {
+                              "Move to EE": (MoveToEEGoal, pp.MoveToEE),
+                              "Move to contact": (MoveToContactActionGoal, pp.MoveToContact),
+                              "UserSync": (UserSyncGoal, pp.UserSync),
+                              "Move fingers": (MoveFingersRequest, pp.MoveFingers),
+                              "Apply force to fingers": (ApplyForceFingersRequest, pp.ApplyForceFingers)
+                              }                    
         self.addButtonActions(self.tabs)
         self.addProgramButtonActions()
         self.addSaveButtonAction()
@@ -40,6 +47,9 @@ class TestWidget(QWidget):
     def addProgramButtonActions(self):
         self.tabs.programPanel.programMenu.buttons[0].pressed.connect(self.addMoveToEE)
         self.tabs.programPanel.programMenu.buttons[1].pressed.connect(self.addMoveToContact)
+        self.tabs.programPanel.programMenu.buttons[2].pressed.connect(self.addUserSync)
+        self.tabs.programPanel.programMenu.buttons[3].pressed.connect(self.addMoveFingers)
+        self.tabs.programPanel.programMenu.buttons[4].pressed.connect(self.addApplyForceFingers)
 
     def saveProgram(self):
         self.program.dump_to_file("~/rqt_ws/src/rqt_mypkg/rqt_practice/Resources", "testprogram.pkl")
@@ -60,6 +70,31 @@ class TestWidget(QWidget):
         self.program.insert_primitive(move_to_contact_primitive, [None, None])
         table = self.tabs.programPanel.programTable.programTable
         self.insertToTable(table, "Move to contact")
+
+    def addUserSync(self):
+        goal = UserSyncGoal()
+        usersync_primitive = pp.UserSync()
+        usersync_primitive.set_parameter_container(goal)
+        self.program.insert_primitive(usersync_primitive, [None, None])
+        table = self.tabs.programPanel.programTable.programTable
+        self.insertToTable(table, "UserSync")
+
+    def addMoveFingers(self):
+        request = MoveFingersRequest()
+        request.width = 100
+        move_fingers_primitive = pp.MoveFingers()
+        move_fingers_primitive.set_parameter_container(request)
+        self.program.insert_primitive(move_fingers_primitive, [None, None])
+        table = self.tabs.programPanel.programTable.programTable
+        self.insertToTable(table, "Move fingers")    
+
+    def addApplyForceFingers(self):
+        goal = ApplyForceFingersRequest()
+        apply_force_fingers_primitive = pp.ApplyForceFingers()
+        apply_force_fingers_primitive.set_parameter_container(goal)
+        self.program.insert_primitive(apply_force_fingers_primitive, [None, None])
+        table = self.tabs.programPanel.programTable.programTable
+        self.insertToTable(table, "Apply force to fingers")      
 
     def insertToTable(self, table, label):
         newItem = QTableWidgetItem(label)
@@ -190,7 +225,7 @@ class ProgramMenu(QWidget):
         self.layout.addWidget(titleText)  
 
     def addProgramButtons(self):
-        labels = ["Move to EE", "Move to contact"]
+        labels = ["Move to EE", "Move to contact", "UserSync", "Move fingers", "Apply force to fingers"]
         for label in labels:
             newButton = QPushButton(label)
             sizePolicy = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Fixed)
